eb1b03fa4af824f1a673c73cfcea319309a9f1ed
Wowfun <haggi.cho@outlook.com>
2020-10-29 09:04:00 +0800

    debug

--- main.py
+++ main.py
@@ -10,13 +10,16 @@ class Args:
     def __init__(self,model_name='lstm'):
         self.model_name=model_name
         self.modes=['train','test','pred']
+        
+        # 数据
         self.dataset='data/train/labeled_data_processed.csv'
-
-        # 文本向量
+        self.val_split=0.1
         self.max_num_words=100000 # 最多保留不同词语词数，频率从大到小
         self.max_sequence_len=256
 
         # 模型超参
+        self.epchos=50
+        self.batch_size=64
         self.embedding_dims=128
 
     @property
@@ -37,10 +40,10 @@ if __name__ == "__main__":
     print('* X shape ',X.shape)
     print('* Y shape ',Y.shape)
     X,_=data_helper.tokenize(X,args.max_num_words,args.max_sequence_len)
-    print('* X shape ',X.shape)
+    print('* vec X shape ',X.shape)
     model1=LSTMModel(args.checkpoint_path)
     model1.build(input_len=X.shape[1],output_units=Y.shape[1])
-    history=model1.train(X,Y)
+    history=model1.train(X,Y,epochs=args.epchos,batch_size=args.batch_size,val_split=args.val_split)
 
 
     plt.title('Loss')
@@ -48,15 +51,15 @@ if __name__ == "__main__":
     plt.plot(history.history['val_loss'], label='val')
     plt.legend()
     # plt.show()
-    plt.savefig('loss.png')
+    plt.savefig('figures/loss.png')
 
     plt.figure()
     plt.title('Accuracy')
-    plt.plot(history.history['acc'], label='train')
-    plt.plot(history.history['val_acc'], label='val')
+    plt.plot(history.history['accuracy'], label='train')
+    plt.plot(history.history['val_accuracy'], label='val')
     plt.legend()
     # plt.show()
-    plt.savefig('acc.png')
+    plt.savefig('figures/accuracy.png')
 
     
 
